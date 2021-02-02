6edae2897ea2ae440f9c9bf3b2371c34bbf50cdf
DeThrelkeld <73961477+DeThrelkeld@users.noreply.github.com>
2021-02-02 13:39:10 -0600

    started question

--- greatBay.js
+++ greatBay.js
@@ -1,4 +1,21 @@
 const mysql = require('mysql');
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const introQ = [
+
+{
+  type: list,
+  name: "intro",
+  message: "Would you like to bid, post or are you done yet?",
+  choices: ["Bid", "Post", "Exit"]
+
+}
+
+];
+
+
 
 const connection = mysql.createConnection({
   host: 'localhost',
