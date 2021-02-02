83b1ca63b81ba2f86ed95dbb8bf4dd179768e440
tafseer-khan <tafseer.k.khan@gmail.com>
2021-02-02 14:05:47 -0600

    Added inquirer

--- greatBay.js
+++ greatBay.js
@@ -2,20 +2,7 @@ const mysql = require('mysql');
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-const introQ = [
-
-{
-  type: list,
-  name: "intro",
-  message: "Would you like to bid, post or are you done yet?",
-  choices: ["Bid", "Post", "Exit"]
-
-}
-
-];
-
-
+const { exit } = require('process');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -31,9 +18,56 @@ const connection = mysql.createConnection({
   database: 'biditems',
 });
 
-connection.connect((err) => {
+
+
+
+function start() {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: "intro",
+      message: "Would you like to bid, post or are you done yet?",
+      choices: ["Bid", "Post", "Exit"]
+
+    }
+  ]).then(function (data) {
+    switch (data.intro) {
+      case "Bid":
+        bid();
+        break;
+      case "Post":
+        post();
+        break;
+      case "Exit":
+        exit();
+        break;
+    }
+  })
+}
+
+const bid = () => {
+  connection.query('SELECT * FROM bidItems', (err, res) => {
     if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-    connection.end();
-  });
-  
+    let bidlist = [];
+    res.forEach(({ item_name }) => {
+      bidlist.push(`${item_name}`)
+    });
+  }).then(  
+    inquirer.prompt([
+    {
+      type: list,
+      name: "bidoptions",
+      message: "Which Item would you like to bid on?",
+      choices: bidlist
+
+    }]))
+    // connection.end();
+}
+start();
+connection.connect((err) => {
+  if (err) throw err;
+  console.log(`connected as id ${connection.threadId}`);
+});
+
+
+
